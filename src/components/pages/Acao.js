deb5ed2a2466a73fb544ff1faa7475d45b7b03c0
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-22 18:07:32 -0300

    Update Acao.js

--- src/components/pages/Acao.js
+++ src/components/pages/Acao.js
@@ -10,93 +10,23 @@ export default function Acao() {
 
         <nav>
       <p><br /></p>
-      <Link to="./Acasadomedo">A casa do medo</Link>
-      <p><br /></p>
-      <Link to="./Acertodecontas">Acerto de contas</Link>
-      <p><br /></p>
-      <Link to="./Agoraestamossos">Agora estamos sos</Link>
-      <p><br /></p>
-      <Link to="./Alemdohorizonte">Além do horizonte</Link>
-      <p><br /></p>
-      <Link to="./Alendadjj">A lenda de Jack e Jill</Link>
-      <p><br /></p>
-      <Link to="./Amorzumbi">Amor Zumbi</Link>
-      <p><br /></p>
-      <Link to="./Anjosdanoite">Anjos da noite</Link>
-      <p><br /></p>
-      <Link to="./Aqualquerpreco">A qualquer preço</Link>
-      <p><br /></p>
       <Link to="./Ascensaoeqdui">Ascensão e queda de um império</Link>
-      <p><br /></p>
-      <Link to="./Assasinatosdopassado">Assasinatos do passado</Link>
-      <p><br /></p>
-      <Link to="./Assasinosdealuguel">Assasinos de aluguel</Link>
       <p><br /></p>
       <Link to="./Batalhadosimperios">Batalha do Imperios</Link>
       <p><br /></p>
       <Link to="./Bloodrayne3">Bloodrayne 3</Link>
       <p><br /></p>
-      <Link to="./Cacadoresderecompensas">Caçadores de recompensas</Link>
-      <p><br /></p>
-      <Link to="./Cargaexplosiva3">Carga explosiva 3</Link>
-      <p><br /></p>
-      <Link to="./Codigodeconduta">Codigo de conduta</Link>
-      <p><br /></p>
-      <Link to="./Criatura">Criatura</Link>
-      <p><br /></p>
       <Link to="./Distrito13o">Distrito 13 o ultimato</Link>
-      <p><br /></p>
-      <Link to="./Embuscadevinganca">Em busca de vingança</Link>
-      <p><br /></p>
-      <Link to="./Entreirmaos">Entre irmãos</Link>
-      <p><br /></p>
-      <Link to="./Ghostland">Ghostland</Link>
       <p><br /></p>
       <Link to="./Homemaranha">Homem Aranha</Link>
       <p><br /></p>
-      <Link to="./Mariasangrenta">Maria Sangrenta</Link>
-      <p><br /></p>
-      <Link to="./Megalodon">Megalodon</Link>
-      <p><br /></p>
-      <Link to="./Necroterio">Necrotério</Link>
-      <p><br /></p>
       <Link to="./Ninja">Ninja</Link>
-      <p><br /></p>
-      <Link to="./Ocaminho">O caminho</Link>
       <p><br /></p>
       <Link to="./Ocladasadagas">O Clã das adagas</Link>
       <p><br /></p>
-      <Link to="./Ocontodoscontos">O Conto dos contos</Link>
-      <p><br /></p>
-      <Link to="./Odespertar">O despertar</Link>
-      <p><br /></p>
       <Link to="./Ohomemdamafia">O homem da mafia</Link>
       <p><br /></p>
-      <Link to="./Oimbativelreden">O imbativel Redenção</Link>
-      <p><br /></p>
-      <Link to="./Olhosfamintos">Olhos famintos</Link>
-      <p><br /></p>
-      <Link to="./Oparamo">O Páramo</Link>
-      <p><br /></p>
-      <Link to="./Osespecialistas">Os especialistas</Link>
-      <p><br /></p>
-      <Link to="./Osmercenarios2">Os mercenarios 2</Link>
-      <p><br /></p>
-      <Link to="./Psicopataamericano2">Psicopata americano 2</Link>
-      <p><br /></p>
-      <Link to="./Residentevilbem">Resident Evil Bem vindo a Raccoon city</Link>
-      <p><br /></p>
-      <Link to="./Redencao">Redenção</Link>
-      <p><br /></p>
       <Link to="./Relacaoexplosiva">Relação explosiva</Link>
-      <p><br /></p>
-      <Link to="./Ruslan">Ruslan</Link>
-      <p><br /></p>
-      <Link to="./Sextafeira13">Sexta feira 13</Link>
-      <p><br /></p>
-      <Link to="./Sonic">Sonic</Link>
-      <p><br /></p>
-      <Link to="./Tempo">Tempo</Link>
       <p><br /></p>
       <Link to="./Umnegocioderisco">Um negocio de risco</Link>
       <p><br /></p>
