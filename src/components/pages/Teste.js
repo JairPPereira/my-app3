28be513c3c1a64b5103715d9210040b9714affd6
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-24 17:11:12 -0300

    Update Teste.js

--- src/components/pages/Teste.js
+++ src/components/pages/Teste.js
@@ -1,77 +1,27 @@
-import React, { Component } from 'react';
-import VideoPlayer from 'react-video-js-player';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './videoapp.css';
 
  
-class Teste extends Component {
-    player = {}
-    state = {
-        video: {
-            src: "http://127.0.0.1:11470/ec7a15a34291a2fe4bc2a924ffc2142015a6b0b9/Godzilla.II.Rei.dos.Monstros.2019.72p.BluRay.6CH.x265.DUAL-WWW.BLUDV.TV.mkv",
-
-            poster: ""
-        }
-    }
- 
-    onPlayerReady(player){
-        console.log("Player is ready: ", player);
-        this.player = player;
-    }
- 
-    onVideoPlay(duration){
-        console.log("Video played at: ", duration);
-    }
- 
-    onVideoPause(duration){
-        console.log("Video paused at: ", duration);
-    }
- 
-    onVideoTimeUpdate(duration){
-        console.log("Time updated: ", duration);
-    }
- 
-    onVideoSeeking(duration){
-        console.log("Video seeking: ", duration);
-    }
- 
-    onVideoSeeked(from, to){
-        console.log(`Video seeked from ${from} to ${to}`);
-    }
- 
-    onVideoEnd(){
-        console.log("Video ended");
-    }
- 
-    render() {
+    export default function Teste() {
         return (
-            <div>
-                      <p><br /></p>
-                <VideoPlayer
-                    controls={true}
-                    src={this.state.video.src}
-                    poster={this.state.video.poster}
-                    width="720px" 
-                    height="auto"
-                    onReady={this.onPlayerReady.bind(this)}
-                    onPlay={this.onVideoPlay.bind(this)}
-                    onPause={this.onVideoPause.bind(this)}
-                    onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
-                    onSeeking={this.onVideoSeeking.bind(this)}
-                    onSeeked={this.onVideoSeeked.bind(this)}
-                    onEnd={this.onVideoEnd.bind(this)}
-                />
-                    <p><br /></p> 
-               <div className="nave">
-      <nav>
+          <div>
+                <p><br /></p>
+            <iframe
+              src="https://www.youtube.com/embed/PLE1UBwJigkZc2Ri5ZMWMZ0vHSPDA9JgRi?autoplay=true"
+              frameborder="0"
+              width="100%" height="320px"
+              allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+              title="video"
+            />{" "}
+                        <div className="nave">
+            <nav>
+            <p><br /></p>
+            <Link to="/">Home</Link>
+      
       <p><br /></p>
-      <Link to="/">Home</Link>
-
-<p><br /></p>
-
-      </nav></div></div>
-            
+      
+            </nav></div></div>
+      
         );
-    }
-}
-export default Teste;
+      }
