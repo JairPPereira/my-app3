6b32001e57cdfb71309efe9ae297aa2f7a688a83
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-23 21:55:03 -0300

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,6 +2,38 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>Ajude a manter nosso aplicativo pelo  pix: jairppereira21@gmail.com
-  </h1>;
+
+  return (
+    <>
+      <h1 className='signUp'>
+
+        <nav>
+      <p><br /></p>
+      <Link to="./Aformadaagua">Aforma da agua</Link>
+      <p><br /></p>
+      <Link to="./Albertnobbs">Albert Nobbs</Link>
+      <p><br /></p>
+      <Link to="./Coragemparaamar">Coragem para amar</Link>
+      <p><br /></p>
+      <Link to="./Entreoamoreafama">Entre o amor e a fama</Link>
+      <p><br /></p>
+      <Link to="./Jogandocomoamor">Jogando com o amor</Link>
+      <p><br /></p>
+      <Link to="./Loucamenteapaixonados">Loucamente apaixonados</Link>
+      <p><br /></p>
+      <Link to="./Umamorprarecordar">Um amor pra recordar</Link>
+      <p><br /></p>
+      <Link to="./Ummatchsurpresa">Um match surpresa</Link>
+      <p><br /></p>
+
+
+
+     
+      </nav></h1>
+
+    </>
+        
+        
+         
+  );
 }
