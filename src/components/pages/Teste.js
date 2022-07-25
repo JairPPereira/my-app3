01b910566dad681a09a7e617aafd0595ea92f684
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-25 19:53:15 -0300

    Update Teste.js

--- src/components/pages/Teste.js
+++ src/components/pages/Teste.js
@@ -8,7 +8,7 @@ import './videoapp.css';
           <div>
                 <p><br /></p>
             <iframe
-              src="https://www.youtube.com/embed/videoseries?list=PLE1UBwJigkZc2Ri5ZMWMZ0vHSPDA9JgRi"
+              src="http://127.0.0.1:11470/2c1e9a29cd7462bed36f69629afd24c3b0a69b09/Ava.2020.720p.BluRay.x264.DUAL-COMANDO.TO.mkv"
               frameborder="0"
               width="100%" height="320px"
               allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
