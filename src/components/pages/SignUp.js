e056e03dbad1c894bd9ed5980256031f05dfd145
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-26 14:26:48 -0300

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,31 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>pix: jairppereira21@gmail.com</h1>;
+  return (
+    <>
+      <h1 className='sign-up'>
+ 
+<nav>
+      <p><br /></p>
+      <Link to="./Aparelhagens">Aparelhagens</Link>
+      <p><br /></p>
+      <Link to="./Atualidade">Atualidade</Link>
+      <p><br /></p>
+      <Link to="./Diario">Diario FM</Link>
+      <p><br /></p>
+      <Link to="./Tophits">Top Hits internacional</Link>
+      <p><br /></p>
+      <Link to="./Vdeoclips">Videos clips</Link>
+
+     
+      </nav></h1>
+
+    </>
+        
+        
+         
+  );
 }
+
+
+
