61c53a81418cbfad71a57e3ff5d40e2b0021cb87
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-26 14:45:51 -0300

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -7,16 +7,6 @@ export default function SignUp() {
       <h1 className='sign-up'>
  
 <nav>
-      <p><br /></p>
-      <Link to="./Aparelhagens">Aparelhagens</Link>
-      <p><br /></p>
-      <Link to="./Atualidade">Atualidade</Link>
-      <p><br /></p>
-      <Link to="./Diario">Diario FM</Link>
-      <p><br /></p>
-      <Link to="./Tophits">Top Hits internacional</Link>
-      <p><br /></p>
-      <Link to="./Vdeoclips">Videos clips</Link>
 
      
       </nav></h1>
