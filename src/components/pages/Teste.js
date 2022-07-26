39bf9886cbd839b5c650c25f28561aa783a66b88
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-26 05:52:30 -0300

    Update Teste.js

--- src/components/pages/Teste.js
+++ src/components/pages/Teste.js
@@ -1,68 +1,19 @@
-import React, { Component } from 'react';
-import VideoPlayer from 'react-video-js-player';
+import React from "react";
 import { Link } from 'react-router-dom';
 import './videoapp.css';
 
- 
-class Teste extends Component {
-    player = {}
-    state = {
-        video: {
-            src: "https://api.prod.gcp.vix.services/play.m3u8?access-key=8722defc5b036e23a1692746719bb1f8adcfa278",
-
-            poster: ""
-        }
-    }
- 
-    onPlayerReady(player){
-        console.log("Player is ready: ", player);
-        this.player = player;
-    }
- 
-    onVideoPlay(duration){
-        console.log("Video played at: ", duration);
-    }
- 
-    onVideoPause(duration){
-        console.log("Video paused at: ", duration);
-    }
- 
-    onVideoTimeUpdate(duration){
-        console.log("Time updated: ", duration);
-    }
- 
-    onVideoSeeking(duration){
-        console.log("Video seeking: ", duration);
-    }
- 
-    onVideoSeeked(from, to){
-        console.log(`Video seeked from ${from} to ${to}`);
-    }
- 
-    onVideoEnd(){
-        console.log("Video ended");
-    }
- 
-    render() {
-        return (
-            <div>
-                      <p><br /></p>
-                <VideoPlayer
-                    controls={true}
-                    src={this.state.video.src}
-                    poster={this.state.video.poster}
-                    width="720px" 
-                    height="auto"
-                    onReady={this.onPlayerReady.bind(this)}
-                    onPlay={this.onVideoPlay.bind(this)}
-                    onPause={this.onVideoPause.bind(this)}
-                    onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
-                    onSeeking={this.onVideoSeeking.bind(this)}
-                    onSeeked={this.onVideoSeeked.bind(this)}
-                    onEnd={this.onVideoEnd.bind(this)}
-                />
-                    <p><br /></p> 
-               <div className="nave">
+export default function Teste() {
+  return (
+    <div>
+          <p><br /></p>
+      <iframe
+        src="https://player.aovivotv.xyz/channels/globonews"
+        frameborder="0"
+        width="100%" height="450px"
+        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+        title="video"
+      />{" "}
+                  <div className="nave">
       <nav>
       <p><br /></p>
       <Link to="/">Home</Link>
@@ -70,8 +21,11 @@ class Teste extends Component {
 <p><br /></p>
 
       </nav></div></div>
-            
-        );
-    }
+
+  );
 }
-export default Teste;
+
+
+
+
+
