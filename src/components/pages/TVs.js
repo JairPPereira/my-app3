ac5166e35ae241e711dc2adfb1baeee01f2f58fe
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-27 16:40:40 -0300

    Update TVs.js

--- src/components/pages/TVs.js
+++ src/components/pages/TVs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function TVs() {
 
@@ -8,37 +8,37 @@ export default function TVs() {
     <>
       <h1 className='tvs'>
 
-<nav>
-      <p><br /></p>
-      <Link to="./Boasnovas">Boas novas</Link>
-      <p><br /></p>
-      <Link to="./Fmodia">FM O Dia</Link>
-      <p><br /></p>
-      <Link to="./Graopara">Grão Pará</Link>
-      <p><br /></p>
-      <Link to="./Rba">Rba</Link>
-      <p><br /></p>
-      <Link to="./Recordnews">Recordnews</Link>
-      <p><br /></p>
-      <Link to="./Globonews">Globonews</Link>
-      <p><br /></p>
-      <Link to="./Bandnews">Bandnews</Link>
-      <p><br /></p>
-      <a href="https://jairppereira.github.io/tvweb/Cnn.html" target="_blank" rel="noopener noreferrer">CNN Brasil</a>
-      <p><br /></p>
-      <a href="http://video01.soultv.com.br/cnnbrasil/cnnbrasil/chunklist_w1048115623.m3u8" target="_blank" rel="noopener noreferrer">CNN Brasil - Apenas para navegadores mobile</a>
-      <p><br /></p>
-      <Link to="./Sbt">Sbt</Link>
-      <p><br /></p>
-      
+        <nav>
+          <p><br /></p>
+          <Link to="./Boasnovas">Boas novas</Link>
+          <p><br /></p>
+          <Link to="./Fmodia">FM O Dia</Link>
+          <p><br /></p>
+          <Link to="./Graopara">Grão Pará</Link>
+          <p><br /></p>
+          <Link to="./Rba">Rba</Link>
+          <p><br /></p>
+          <Link to="./Recordnews">Recordnews</Link>
+          <p><br /></p>
+          <Link to="./Globonews">Globonews</Link>
+          <p><br /></p>
+          <Link to="./Bandnews">Bandnews</Link>
+          <p><br /></p>
+          <a href="https://jppfilmesonline.blogspot.com/p/untitled-1-download-video-file.html" target="_blank" rel="noopener noreferrer">CNN Brasil</a>
+          <p><br /></p>
+          <a href="http://video01.soultv.com.br/cnnbrasil/cnnbrasil/chunklist_w1048115623.m3u8" target="_blank" rel="noopener noreferrer">CNN Brasil - Apenas para navegadores mobile</a>
+          <p><br /></p>
+          <Link to="./Sbt">Sbt</Link>
+          <p><br /></p>
 
-     
-      </nav></h1>
+
+
+        </nav></h1>
 
     </>
-        
-        
-         
+
+
+
   );
 }
 
