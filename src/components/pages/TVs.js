511ae09b6a7362264fa046b0a4963355e2c85adb
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-27 12:22:44 -0300

    Update TVs.js

--- src/components/pages/TVs.js
+++ src/components/pages/TVs.js
@@ -26,6 +26,8 @@ export default function TVs() {
       <p><br /></p>
       <Link to="./Cnn">CNN Brasil</Link>
       <p><br /></p>
+      <a href="https://jairppereira.github.io/tvweb/Cnn.html" target="_blank" rel="noopener noreferrer">CNN Brasil - teste</a>
+      <p><br /></p>
       <a href="http://video01.soultv.com.br/cnnbrasil/cnnbrasil/chunklist_w1048115623.m3u8" target="_blank" rel="noopener noreferrer">CNN Brasil - Apenas para navegadores mobile</a>
       <p><br /></p>
       <Link to="./Sbt">Sbt</Link>
