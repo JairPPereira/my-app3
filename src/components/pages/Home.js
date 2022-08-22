ab849d05a619ba8347dd48b47e3bf1ec40660a2f
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-08-22 14:45:53 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -63,7 +63,8 @@ export default function Home() {
           
   
         </nav>
-
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8092263721786278"
+     crossorigin="anonymous"></script>
     <Container fluid="md">
       <Row>
         <Col>Ajude a manter nosso site</Col>
