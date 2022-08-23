44a198830e1b78bdb56dfcc22d30e79b5c5cfcf6
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-08-23 08:40:23 -0300

    Update Tvab.js

--- src/components/pages/Tvab.js
+++ src/components/pages/Tvab.js
@@ -11,45 +11,8 @@ export default function Tvab() {
 
   return (
     <>
-      <h1 className='tvs'>
-
-        <nav>
-          <p><br /></p>
-          <Card>
-      <Card.Body><Link to="./Boasnovas"><Button variant="primary" size="lg" width="100%">
-      Boas novas
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-    <Card.Body><Link to="./Fmodia"><Button variant="primary" size="lg" width="100%">
-    FM O Dia
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-    <Card.Body><Link to="./Graopara"><Button variant="primary" size="lg" width="100%">
-    Grão Pará
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-    <Card.Body><Link to="./Teste2"><Button variant="primary" size="lg" width="100%">
-    Teste1
-        </Button></Link></Card.Body>
-    </Card>
-
-
-    <Card>
-    <Card.Body><a href="http://www.portalcultura.com.br/playerhtml/index.html"><Button variant="primary" size="lg" width="100%">
-    Teste2
-        </Button></a></Card.Body>
-    </Card>
-
-
-
-          <p><br /></p>
-
-
-
-        </nav></h1>
+     <p><br /></p>
+      
         <center>
           <p></p>
       <Card style={{ width: '18rem' }}>
@@ -120,16 +83,31 @@ export default function Tvab() {
       </Card.Body>
     </Card></center>
 
-       
     <center>
           <p></p>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS7-jt3Yh-cJqBk_en593s7_dv-yWG7xfnQv27bq0ZoyM9Dk9nRR9tWYbJ1hn-G53_OgE&usqp=CAU" />
+      <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/pt/3/39/Logotipo_Rede_Cultura_do_Par%C3%A1.png" />
       <Card.Body>
-        <Card.Title>ITV TV</Card.Title>
-        <Card.Text><p2>ITV Channel que pode ser uma mão na roda pra você gostaria de ver alguns canais de TV que não estão dísponivéis no Rio de Janeiro ou em qualquer outro lugar do Brasil.</p2>
+        <Card.Title>Cultura Pará</Card.Title>
+        <Card.Text><p2>Rede Cultura do Pará é uma emissora de televisão brasileira sediada em Belém, capital do estado do Pará. Opera no canal 2, e é afiliada à TV Cultura.</p2>
         </Card.Text>
-        <Link to="./Itvtv"><Button variant="primary" size="lg" width="100%">
+        <a href="http://www.portalcultura.com.br/playerhtml/index.html"><Button variant="primary" size="lg" width="100%"><p1>Assistir</p1>
+        
+        </Button></a>
+      </Card.Body>
+    </Card></center>
+
+
+
+    <center>
+          <p></p>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://www.tvgraopara.com.br/wp-content/uploads/bfi_thumb/download-nzlm2lmoj7gnrngw62qn5x5i5erjyxmrily4z26zn0.jpg" />
+      <Card.Body>
+        <Card.Title>TV Grão Pará</Card.Title>
+        <Card.Text><p2>A TV Grão Pará é uma emissora de televisão brasileira sediada na cidade de Belém, no estado do Pará. Opera no canal 14 e é afiliada à TV Gazeta.</p2>
+        </Card.Text>
+        <Link to="./Graopara"><Button variant="primary" size="lg" width="100%">
       Assistir
         </Button></Link>
       </Card.Body>
@@ -138,40 +116,27 @@ export default function Tvab() {
     <center>
           <p></p>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://play-lh.googleusercontent.com/gIiZK9ZUU7tt8N08ozfe8OPBnW-KPREAHdbN1WDvqqSscVdAQuuP2_i2c1Gm2zHJ_S9x" />
+      <Card.Img variant="top" src="https://img.radios.com.br/radio/xl/radio13213_1559217338.png" />
       <Card.Body>
-        <Card.Title>Pluto TV</Card.Title>
-        <Card.Text><p2>A Pluto TV é uma aplicação que lhe permite ver mais de 100 canais de televisão portáteis para a Internet, gratuitamente em qualquer dispositivo, em qualquer lugar. Com este aplicativo, você também pode transmitir Pluto TV via Google Chromecast e assistir à oferta de TV em uma grande TV.</p2>
+        <Card.Title>FM O Dia</Card.Title>
+        <Card.Text><p2>FM O Dia é uma rede de rádios brasileira com sede no município do Rio de Janeiro, capital do estado homônimo. Opera nos 100,5 MHz em FM.</p2>
         </Card.Text>
-        <Link to="./Canaisp"><Button variant="primary" size="lg" width="100%">
+        <Link to="./Fmodia"><Button variant="primary" size="lg" width="100%">
       Assistir
         </Button></Link>
       </Card.Body>
     </Card></center>
 
-    <center>
-          <p></p>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://www.runtime.tv/themes/custom/run/runtime/logo.png" />
-      <Card.Body>
-        <Card.Title>Runtime TV</Card.Title>
-        <Card.Text><p2>Runtime é onde você encontra filmes e séries premiadas sempre grátis.</p2>
-        </Card.Text>
-        <Link to="./Canaisrun"><Button variant="primary" size="lg" width="100%">
-      Assistir
-        </Button></Link>
-      </Card.Body>
-    </Card></center>
 
     <center>
           <p></p>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://pbs.twimg.com/profile_images/1461742005176909838/mSlnGpF1_400x400.jpg" />
+      <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/pt/3/39/Rede_Boas_Novas.png" />
       <Card.Body>
-        <Card.Title>Disney Channel Brasil</Card.Title>
-        <Card.Text><p2></p2>
+        <Card.Title>TV Boas Novas</Card.Title>
+        <Card.Text><p2>Boas Novas é uma rede de televisão brasileira pertencente ao pastor Samuel Câmara, que em 1993, comprou a Rede Brasil Norte do Grupo Simões, para transformar em Boas Novas em Manaus.</p2>
         </Card.Text>
-        <Link to="./Disneyc"><Button variant="primary" size="lg" width="100%">
+        <Link to="./Boasnovas"><Button variant="primary" size="lg" width="100%">
       Assistir
         </Button></Link>
       </Card.Body>
