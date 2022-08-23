a064866fad860eddb315abb47b06fe255433e613
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-08-22 23:11:16 -0300

    Update Disneyc.js

--- src/components/pages/Disneyc.js
+++ src/components/pages/Disneyc.js
@@ -7,7 +7,7 @@ export default function Disneyc() {
     <div>
           <p><br /></p>
       <iframe
-        src="https://player.aovivotv.xyz/channels/disneychannel/external"
+        src="https://canais.m3u8live.ml/v.php?id=disneychannel"
         target="_blank"
         frameborder="0"
         width="100%" height="600px"
